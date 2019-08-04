2cc222633ecf4ef7f4f86289545a9303a90fd393
Jaakko Kenttä <kenttajaa@gmail.com>
2019-08-04 13:20:46 +0300

    JS

--- JavaScript/Which-are-in?/solution.js
+++ JavaScript/Which-are-in?/solution.js
@@ -1,5 +1,7 @@
 /*
-Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+Given two arrays of strings a1 and a2 return a sorted array r in 
+lexicographical order of the 
+strings of a1 which are substrings of strings of a2.
 
 #Example 1: 
 a1 = ["arp", "live", "strong"]
@@ -12,9 +14,23 @@ a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
 returns []
 */
 
-console.log("hello");
 
 exports.inArray = function(array1, array2) {
+    if (!array1 || !array2) return []
 
-    return []
+    let returnValues = []
+
+    for (let i = 0; i < array1.length; i++) {
+        let word = array1[i]
+
+        for (let j = 0; j < array2.length; j++) {
+            let word2 = array2[j];
+            if (word2.includes(word) && !returnValues.includes(word)) {
+                returnValues.push(word)
+            }
+        }
+        
+    }
+
+    return returnValues
 }
--- JavaScript/Which-are-in?/test.js
+++ JavaScript/Which-are-in?/test.js
@@ -1,13 +1,48 @@
-var assert = require('assert');
-var inArray = require('./solution');
+var expect = require('chai').expect;
+var should = require('chai').should;
+
+var inArray = require('./solution').inArray;
 
 
 describe('inArrayTests', function () {
- it('should return an empty array if arguments are not provided', function () {
-        assert.equal("Hello".length, 4);
-        assert.equal(inArray(), []);
+
+    it('should return an empty array with no arguments', function () {    
+        var expectedResult = [];
+        var returnValue = inArray();
+
+        expect(returnValue).is.eql(expectedResult);
     });
- it('should returns same array if two same array is provided', function () {
-        assert.equal("Hello".charAt(0), 'H');
+
+    it('should returns same array with two same arrays', function () {
+        var a1 = ["2"];
+        var a2 = ["2"];
+        var result = ["2"];  
+
+        var returnValue = inArray(a1, a2);
+
+        expect(inArray(["2"], ["2"])).is.eql(["2"]);
     });
+
+    it('should return items exact match', function() {
+        let returnValue = inArray(["ok"], ["yep", "no", "ok"]);
+        expect(returnValue).is.eql(["ok"]);
+    });
+
+    it('should return all substrings without duplicates', function() {
+        var a1 = ["arp", "live", "nice"];
+        var a2 = ["harp", "sharp", "lively"];
+        var result = ["arp", "live"];  
+
+        var returnValue = inArray(a1, a2);
+        expect(returnValue).is.eql(result);
+    });
+
+    it('should return in right order', function() {
+        var a1 = ["abc", "def", "ghj"];
+        var a2 = ["abc", "ghj", "def"];
+        var result = a1
+
+        var returnValue = inArray(a1, a2);
+        expect(returnValue).is.eql(result);
+    })
 });
